273ac6bea3ad8f4c6f8e75e02a0bb27ff4aea275
Nevin <nevviin@gmail.com>
2021-01-25 09:27:56 +1100

    code snippet to redirect the api root  url to swagger by default

--- BattleShip/BattleShip.Api/Startup.cs
+++ BattleShip/BattleShip.Api/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -65,11 +66,18 @@ namespace BattleShip.Api
             }
             app.UseSwagger();
 
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BattleShip.Api v1"));
+            app.UseSwaggerUI(
+                c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BattleShip.Api v1");
+                    //c.RoutePrefix = string.Empty;
+                }
+                ); ;
             app.UseRouting();
 
             app.UseAuthorization();
-
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
